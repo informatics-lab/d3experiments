94b50bc6e18b949e331d8086451e62f454b3f53d
Niall Robinson <niall.h.robinson@gmail.com>
2015-08-12 16:40:46 +0100

    sped up tweens

--- js/main.js
+++ js/main.js
@@ -12,9 +12,6 @@ loadData = function(){
   });
 };
 
-function update() {
-    
-}
 
 horizBarChart = function(allData){
     var data = allData;
@@ -30,26 +27,30 @@ horizBarChart = function(allData){
     var chronologicalOrder = function(a, b) {return a.year - b.year;}
     var temperatureOrder = function(a, b) {return a.temp - b.temp;}
 
+    var reorder = function(order){
+        svg.selectAll(".bar")
+           .sort(order)
+        svg.transition()
+            .duration(400)
+            .selectAll(".bar")
+            .delay(function(d, i){ return i * 1000 / data.length; })
+            .attr("y", function(d, i){ return barOrder(i) })
+    }
+
     d3.select("body")
       .append("label")
         .attr("id", "sort")
         .html("<input id=sortCheck type=checkbox> sort years")
       .on("change", function(){
-        svg.selectAll(".bar")
-           .sort(d3.select("#sortCheck").property("checked") ? temperatureOrder : chronologicalOrder)
-        svg.transition()
-            .duration(200)
-            .selectAll(".bar")
-            .delay(function(d, i){ return i * 30; })
-            .attr("y", function(d, i){ return barOrder(i) })
+           reorder(d3.select("#sortCheck").property("checked") ? temperatureOrder : chronologicalOrder)
       })
 
     d3.select("#filterData")
         .on("click", function(d) {
-            var minTemp = d3.select("#minTemp").property("value")
-            var maxTemp = d3.select("#maxTemp").property("value")
-            data = allData.filter(function(d){return d.temp > minTemp && d.temp < maxTemp})
-
+            var minTemp = d3.select("#minTemp").property("value");
+            var maxTemp = d3.select("#maxTemp").property("value");
+            data = allData.filter(function(d){return d.temp >= minTemp && d.temp < maxTemp});
+            update(data);
          })
 
     // width = 100;
@@ -68,39 +69,57 @@ horizBarChart = function(allData){
                     .attr("class", "tooltip") // apply the 'tooltip' class
                     .style("opacity", 0);
 
-    svg.selectAll(".bar")
-        .data(data)
-            .enter()
-                .append("rect")
-                    .attr("class", "bar")
-                    .attr("x", function(d) {return w / 2 + Math.min(0.0, barLength(d.temp));})
-                    .attr("y", function(d, i){ return barOrder(i) })
-                    .attr("height", (h / data.length) * (1.0-barPadding))
-                    .attr("fill", function(d) {
-                        var rgbstr = ""
-                        if (d.temp <= 0.0){
-                            rgbstr = "rgb(0, 0, "+ Math.round(colorIntensity(Math.abs(d.temp))) +")";
-                        }else{
-                            rgbstr = "rgb("+ Math.round(colorIntensity(Math.abs(d.temp))) +", 0, 0)";
-                        }
-                        return rgbstr
-                    })
-                    .attr("width", function(d) { return Math.abs(barLength(d.temp)); })
-                    .on("mouseover", function(d){
-                        tooltip.transition()
-                                    .duration(500)
-                                    .style("opacity", 0.9)
-                        tooltip.html(d.year + "<br />" + d.temp + " &deg;C")
-                                .style("left", (d3.event.pageX) + "px")          
-                                .style("top", (d3.event.pageY - 28) + "px");
-                    })
-                    .on("mouseout", function(d){
-                        tooltip.transition()
-                                    .duration(500)
-                                    .style("opacity", 0.0)
-                    })
+    function update(data) {
+        var bar = svg.selectAll(".bar").data(data, function(d){return d.year})
+        
+        bar.enter()
+            .append("rect")
+                .attr("class", "bar")
+                .attr("y", function(d, i){ return barOrder(i) })
+                .attr("height", (h / data.length) * (1.0-barPadding))
+                .attr("width", 0)
+                .attr("x", function(d) {return w / 2;})
+                    .transition()
+                    .delay(function(d, i) { return i * 400 / data.length; })
+                        .duration(500)
+                        .attr("width", function(d) { return Math.abs(barLength(d.temp)); })
+                        .attr("x", function(d) {return w / 2 + Math.min(0.0, barLength(d.temp));})
+                .attr("fill", function(d) {
+                    var rgbstr = ""
+                    if (d.temp <= 0.0){
+                        rgbstr = "rgb(0, 0, "+ Math.round(colorIntensity(Math.abs(d.temp))) +")";
+                    }else{
+                        rgbstr = "rgb("+ Math.round(colorIntensity(Math.abs(d.temp))) +", 0, 0)";
+                    }
+                    return rgbstr
+                });
+        
+        bar.attr("y", function(d, i){ return barOrder(i) })
+            .on("mouseover", function(d){
+                    tooltip.transition()
+                                .duration(500)
+                                .style("opacity", 0.9)
+                    tooltip.html(d.year + "<br />" + d.temp + " &deg;C")
+                            .style("left", (d3.event.pageX) + "px")          
+                            .style("top", (d3.event.pageY - 28) + "px");
+                })
+            .on("mouseout", function(d){
+                    tooltip.transition()
+                                .duration(500)
+                                .style("opacity", 0.0)
+                });
+        
+        bar.exit()
+            .transition()
+                .duration(1000)
+                .attr("width", 0)
+                .attr("x", function(d) {return w / 2;})
+            .remove()
 
     }
 
+
+    update(data);
+}
 
 loadData();
